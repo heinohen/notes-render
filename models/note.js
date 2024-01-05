3a7f19770be34eb83274c8abfaa27b6cf9a70543
Henrik Heinonen <hohein@utu.fi>
2024-01-05 14:59:22 +0200

    refractor

--- models/note.js
+++ models/note.js
@@ -1,21 +1,5 @@
 const mongoose = require('mongoose')
 
-mongoose.set('strictQuery', false)
-
-const password = "VitsinVimpulaSalasana1"
-const nameOfDB = "noteApp"
-const url = process.env.MONGODB_URI
-//`mongodb+srv://mongohenkka:${password}@isoklusteri.ms3ihqf.mongodb.net/${nameOfDB}?retryWrites=true&w=majority`
-console.log('connecting to', url)
-// Yhteyden luonti
-mongoose.connect(url)
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB', error.message)
-    })
-
 const noteSchema = new mongoose.Schema({
     content: String,
     important: Boolean,
